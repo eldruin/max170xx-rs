e3633c631b87431622850520891a12624522fc10
Diego Barrios Romero <eldruin@gmail.com>
2020-04-18 13:27:13 +0200

    Add linux example

---
+++ examples/linux.rs
@@ -0,0 +1,11 @@
+extern crate linux_embedded_hal as hal;
+use max1704x::Max1704x;
+
+fn main() {
+    let dev = hal::I2cdev::new("/dev/i2c-1").unwrap();
+    let mut sensor = Max1704x::new_max17043_4(dev);
+    loop {
+        let soc = sensor.soc().unwrap();
+        println!("Charge: {:.2}%", soc);
+    }
+}
