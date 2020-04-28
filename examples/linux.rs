1d1f7ed8722d53304a934e38babae0761494b899
Diego Barrios Romero <eldruin@gmail.com>
2020-04-28 19:38:10 +0200

    Simplify code

--- examples/linux.rs
+++ examples/linux.rs
@@ -1,8 +1,8 @@
-extern crate linux_embedded_hal as hal;
+use linux_embedded_hal::I2cdev;
 use max170xx::Max17043;
 
 fn main() {
-    let dev = hal::I2cdev::new("/dev/i2c-1").unwrap();
+    let dev = I2cdev::new("/dev/i2c-1").unwrap();
     let mut sensor = Max17043::new(dev);
     loop {
         let soc = sensor.soc().unwrap();
